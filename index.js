32c68f07537d705f2bfbf4bb934d3ddc9d93107a
renantescaro <renantescaro@yahoo.com.br>
2021-05-05 14:17:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 
 window.onload = function(){
-    
-    var dvRetorno = document.getElementById('dvRetorno');
-    
-    var get = new XMLHttpRequest();
+    var dvRetorno = document.getElementById('dvRetorno')
+    var get = new XMLHttpRequest()
 
-    get.open("GET", "https://viacep.com.br/ws/01001000/json/", true);
-
-    get.send();
-
+    get.open("GET", "https://viacep.com.br/ws/01001000/json/", true)
+    get.send()
     get.onreadystatechange = function(){
-    
-        if (get.readyState == 4 && get.status == 200) {
-        
-            dvRetorno.innerText = get.responseText;
+        if (get.readyState == 4 && get.status == 200){
+            dvRetorno.innerText = get.responseText
         }
     }
 }
